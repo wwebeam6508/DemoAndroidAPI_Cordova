de6b41b1a834bc40fd5df31aabde0c45e026b95f
policebeam <policebeam@gmail.com>
2018-10-09 10:01:27 +0700

    updategpio

--- src/android/NativeConnector.java
+++ src/android/NativeConnector.java
@@ -1,6 +1,6 @@
 package cordova.plugin.nativeconnector;
 import com.nativecode.serialport.keycardClass;
-
+import com.nativecode.serialport.serialport.GPIOClass;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
@@ -14,40 +14,52 @@ import org.json.JSONObject;
  * This class echoes a string called from JavaScript.
  */
 public class NativeConnector extends CordovaPlugin {
-    keycardClass keycard =  new keycardClass();
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if(action.equals("sendValue"))
-        {
-            this.sendValue(args, callbackContext);
-            return true;
-        }else if(action.equals("getGPSValue")){
-          this.getGPSValue(callbackContext);
-          return true;
-        }
-        return false;
+  keycardClass keycard =  new keycardClass();
+  GPIOClass gpio = new GPIOClass();
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if(action.equals("sendValue"))
+    {
+      this.sendValue(args, callbackContext);
+      return true;
+    }else if(action.equals("openKeycardPort")){
+      this.openKeycardPort(callbackContext);
+      return true;
+    }else if(action.equals("openGPIOPort")){
+      this.openGPIOPort(callbackContext);
+      return true;
     }
+    return false;
+  }
 
-    private void sendValue(JSONArray args,CallbackContext callback){
-        if(args != null){
-            try{
-                String text1 = args.getJSONObject(0).getString("param1");
-                callback.success(text1);
-            }catch(Exception ex){
-                callback.error("I don't Know" + ex);
-            }
-        }else{
-            callback.error("Please Insert PLEASEEEEEE!");
-        }
-    }
-
-    private void getGPSValue(CallbackContext callback){
-
-        try{
-          keycard.init(callback);
-        }catch(Exception ex){
-          callback.error("I don't Know" + ex);
-        }
+  private void sendValue(JSONArray args,CallbackContext callback){
+    if(args != null){
+      try{
+        String text1 = args.getJSONObject(0).getString("param1");
+        callback.success(text1);
+      }catch(Exception ex){
+        callback.error("I don't Know" + ex);
       }
+    }else{
+      callback.error("Please Insert PLEASEEEEEE!");
+    }
+  }
 
+  private void openKeycardPort(CallbackContext callback){
+
+    try{
+      keycard.init(callback);
+    }catch(Exception ex){
+      callback.error("I don't Know" + ex);
+    }
+  }
+
+  private void openGPIOPort(CallbackContext callback){
+    
+    try{
+      gpio.init(callback);
+    }catch(Exception ex){
+      callback.error("I don't Know" + ex);
+    }
+  }
 }
--- www/NativeConnector.js
+++ www/NativeConnector.js
@@ -1,9 +1,17 @@
-var exec = require('cordova/exec');
-
-module.exports.sendValue = function (arg0, success, error) {
-    exec(success, error, 'NativeConnector', 'sendValue', [arg0]);
-};
-
-module.exports.getGPSValue = function ( success, error) {
-    exec(success, error, 'NativeConnector', 'getGPSValue', []);
-};
+cordova.define("cordova-plugin-nativeconnector.NativeConnector", function(require, exports, module) {
+    var exec = require('cordova/exec');
+    
+    module.exports.sendValue = function (arg0, success, error) {
+        exec(success, error, 'NativeConnector', 'sendValue', [arg0]);
+    };
+    
+    module.exports.openKeycardPort = function ( success, error) {
+        exec(success, error, 'NativeConnector', 'openKeycardPort', []);
+    };
+    
+    module.exports.openGPIOPort = function ( success, error) {
+        exec(success, error, 'NativeConnector', 'openGPIOPort', []);
+    };
+    
+    });
+    
