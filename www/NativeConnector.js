e9f85d802d35b37b2f787653441c73c7b5b9c185
policebeam <policebeam@gmail.com>
2018-10-19 15:30:07 +0700

    fix www nativeconnector.js

--- www/NativeConnector.js
+++ www/NativeConnector.js
@@ -1,4 +1,4 @@
-cordova.define("cordova-plugin-nativeconnector.NativeConnector", function(require, exports, module) {
+
 var exec = require('cordova/exec');
 
 module.exports.sendValue = function (arg0, success, error) {
@@ -21,4 +21,4 @@ module.exports.closeGPIOPort = function ( success, error) {
     exec(success, error, 'NativeConnector', 'closeGPIOPort', []);
 };
 
-});
+
